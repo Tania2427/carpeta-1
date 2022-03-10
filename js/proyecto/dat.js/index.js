0104cdc9abcfe32fa8dbc54049d4a798ec29edd9
Tania2427 <taniaguimeragarcia@gmail.com>
2022-03-10 13:34:37 +0100

    exercicis botiga

---
+++ js/proyecto/dat.js/index.js
@@ -0,0 +1,10 @@
+function buy (id) {
+  
+console.table(products);
+console.log(cartList);
+cart.push(products[id -1]);
+
+/*const addproduct = products.find(element => element.id == id);
+cart.push (addproduct );*/
+ 
+}
