b7d2c4b9758be3489c6488977b5ec445314a1558
Tania2427 <taniaguimeragarcia@gmail.com>
2022-03-15 12:23:26 +0100

    exeercicis java moderno

--- js/javascript modern/js/films.js
+++ js/javascript modern/js/films.js
@@ -17,11 +17,18 @@
      return result2;
     // console.table(result2);
  }
+ 
+ /*exercici3:Per a tenir més informació d'aquest director, es demana calcular la mitjana de les
+puntuacions de les seves pel·lícules.Implementar la funció pelisAverage(), la qual rep un array de pel·lícules
+i retorna la nota mitjana, amb dos decimals.*/
 
- //exercici3:
+ function pelisAverage (nombreDirector) {
+     let filmsPelisDirector= getPelisFromDirector(nombreDirector);
+     let conteo= filmsPelisDirector.reduce ((conteoActual,filmsPelisDirector) => { //conteo:es una acumulacion y filmsPelisDirector es el valor que esta tomando la función.
+       conteoActual[filmsPelisDirector]=(conteoActual[filmsPelisDirector]|| 0) +1;
+      console.table( conteoActual);
+      },{}); 
+    }
+ console.table(filmsPelisDirector);
 
- /*function moviesAverage (director) {
-     let result2= getPelisFromDirector(director);
-     let scoreAverage=
-     console.table(result2);
- }*/
+ 
