dc9d69b1ac0edebcd0f20177df8b211e21c5989f
Tania2427 <taniaguimeragarcia@gmail.com>
2022-03-30 21:55:11 +0200

    exercicis Vue

---
+++ Vue/interpolacion/js/exercici01.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: "#app",
+    template: "#ejemplo",
+    data: {
+        mensaje: "Bienvenido a mi mundo",
+      // mensajeHTML: "esto es un mensaje HTML",
+        //imagen: "imagen/atmella de mar.jpg",
+        url: "https://www.uoc.",
+        ok: true,
+        number: 12
+    }
+});
