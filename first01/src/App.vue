dcd5cbf3c0c4addf8c1c3a97469922001e0beff3
Tania2427 <taniaguimeragarcia@gmail.com>
2022-04-26 10:12:16 +0200

    exercicix vue

---
+++ first01/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/01.jpg">
+  <HelloWorld msg="Bienvenidos a mi casa encantada"/>
+  <NewComponents msg="Bienvenidos "/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NewComponents from './components/NewComponents.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ novapp/src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ acudit }}
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "AboutView",
+  data() {
+    return {
+      post: null,
+      acudit: "",
+    };
+  },
+  mounted() {
+    console.log("entra");
+    axios
+      .get("https://api.chucknorris.io/jokes/random")
+      .then((response) => (this.acudit = response.data.value));
+  },
+};
+</script>
